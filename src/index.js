b136d37853aa8cf416967422ba6a47ba23ce531b
ClassicalJo <jb.barrientos@yahoo.com>
2020-02-16 11:22:43 +1100

    Removed thunk
    Thunk was generating problems on android, and I'm not really sending functions through redux. So i can safely remove it until i have need of it

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import rootReducer from './reducers/Reducers'
 
 const STORE = createStore(rootReducer,
     compose(
-        applyMiddleware(thunk),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     ));
 
